543da4d60bd91c83cfd47ab8b4c485acbd428e82
ya7ya99 <135076559+ya7ya99@users.noreply.github.com>
2023-12-09 11:54:33 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -7,9 +7,8 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """Amenity class that inherits from BaseModel."""
-    name = ""
-
+    
     def __init__(self, *args, **kwargs):
         """Initialize a new instance with a unique id."""
         super().__init__()
-
+        self.name = ""
